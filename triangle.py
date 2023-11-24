37037eb4a398744c0716c3d35fed57bf5e2f11a1
DoniyorSayfiddinov <sayfiddinovdoniyor833@gmail.com>
2023-11-24 12:08:14 +0500

    ishlandi

--- triangle.py
+++ triangle.py
@@ -1,10 +1,10 @@
 from polygon import Polygon
 from math import sqrt
 class Triangle(Polygon):
-    def init(self, height, width, c) -> None:
-        super().init(height,width)
+    def __init__(self, height, width, c) -> None:
+        super().__init__(height,width)
         self.c=c
-    def getArea(self):
+    def getArea(self): 
         p=(self.height+self.width+self.c)/2
         return sqrt(p*(p-self.height)*(p-self.width)*(p-self.c)) if self.height>0 and self.width>0 and self.c>0 and (self.height+self.width)>self.c and (self.height+self.c)>self.width and (self.width+self.c)>self.height else "Error"
     def getPerimeter(self):
